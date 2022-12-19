b5c44068631dad82b3c9a1290a78249ebfbc3948
mskim <mskim@surromind.ai>
2022-12-19 07:09:31 +0000

    init_

--- .ipynb_checkpoints/start-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo date
-date
-echo ## start of training
-echo CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 train.py --dataset custom --split overlap --fp16 --name sample_run
-CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 train.py --dataset custom --split overlap --fp16 --name sample_run 
-echo ## end of training
-echo date
-date
